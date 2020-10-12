1c99bfe3ef44167e6a51948d09b3b952430a9592
Alex <alx.bulla@gmail.com>
2020-10-12 22:30:16 +0200

    Added controls to the dice select example.

--- examples/DiceSelect.elm
+++ examples/DiceSelect.elm
@@ -1,10 +1,13 @@
 module DiceSelect exposing (..)
 
 import Browser
+import Browser.Events exposing (onKeyDown)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import Random exposing (int)
+import Json.Decode as Decode exposing (Decoder)
+import MultiSelectRing exposing (MultiSelectRing)
+import Random
 
 
 
@@ -25,13 +28,22 @@ main =
 
 
 type alias Model =
-    { dice : List Int
+    { dice : MultiSelectRing Die
     }
+
+
+type Die
+    = One
+    | Two
+    | Three
+    | Four
+    | Five
+    | Six
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model []
+    ( { dice = MultiSelectRing.empty }
     , Random.generate DiceRolled fiveRandomDice
     )
 
@@ -42,7 +54,16 @@ init _ =
 
 type Msg
     = RollDice
-    | DiceRolled (List Int)
+    | DiceRolled (List Die)
+    | DieClicked Die
+    | ButtonPressed Button
+
+
+type Button
+    = LeftArrow
+    | RightArrow
+    | Space
+    | Enter
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -54,23 +75,132 @@ update msg model =
             )
 
         DiceRolled dice ->
-            ( { dice = dice }
+            ( rollDice dice model
+            , Cmd.none
+            )
+
+        DieClicked die ->
+            ( toggleDie die model
+            , Cmd.none
+            )
+
+        ButtonPressed button ->
+            ( applyButtonPress button model
             , Cmd.none
             )
 
 
-fiveRandomDice : Random.Generator (List Int)
+fiveRandomDice : Random.Generator (List Die)
 fiveRandomDice =
-    Random.list 5 (Random.int 1 6)
+    Random.int 1 6
+        |> Random.list 5
+        |> Random.map (\dice -> List.map intToDie dice)
+
+
+rollDice : List Die -> Model -> Model
+rollDice dice model =
+    { model
+        | dice = MultiSelectRing.fromList dice
+    }
+
+
+{-| Focus on the previous item.
+-}
+focusOnPreviousItem : Model -> Model
+focusOnPreviousItem model =
+    { model
+        | dice = MultiSelectRing.focusOnPrevious model.dice
+    }
+
+
+{-| Focus on the next item.
+-}
+focusOnNextItem : Model -> Model
+focusOnNextItem model =
+    { model
+        | dice = MultiSelectRing.focusOnNext model.dice
+    }
+
+
+{-| Toggle selection of the provided die.
+-}
+toggleDie : Die -> Model -> Model
+toggleDie toggledItem model =
+    { model
+        | dice =
+            model.dice
+                |> MultiSelectRing.focusOnFirstMatching (\item -> item == toggledItem)
+                |> MultiSelectRing.toggleFocused
+    }
+
+
+{-| Toggle selection of the currently focused item.
+-}
+toggleFocused : Model -> Model
+toggleFocused model =
+    { model
+        | dice = MultiSelectRing.toggleFocused model.dice
+    }
+
+
+{-| Apply the corresponding action in response of a key pressed.
+-}
+applyButtonPress : Button -> Model -> Model
+applyButtonPress button model =
+    case button of
+        LeftArrow ->
+            focusOnPreviousItem model
+
+        RightArrow ->
+            focusOnNextItem model
+
+        Space ->
+            toggleFocused model
+
+        Enter ->
+            toggleFocused model
 
 
 
 -- SUBSCRIPTIONS
 
 
+{-| Subscribe to the key pressed events.
+-}
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Sub.batch
+        [ onKeyDown (Decode.map ButtonPressed decodeButton)
+        ]
+
+
+{-| Decode a key related event into its corresponding Button event.
+-}
+decodeButton : Decoder Button
+decodeButton =
+    Decode.field "key" Decode.string
+        |> Decode.andThen toButton
+
+
+{-| Convert a key name into its corresponding Button.
+-}
+toButton : String -> Decoder Button
+toButton key =
+    case key of
+        "ArrowLeft" ->
+            Decode.succeed LeftArrow
+
+        "ArrowRight" ->
+            Decode.succeed RightArrow
+
+        " " ->
+            Decode.succeed Space
+
+        "Enter" ->
+            Decode.succeed Enter
+
+        _ ->
+            Decode.fail "Skip"
 
 
 
@@ -83,7 +213,7 @@ view model =
         [ h1 [ style "padding" "12px" ]
             [ text "Pick a few dice" ]
         , div [ style "padding" "0 12px" ]
-            (List.map dieFaceImage model.dice)
+            [ diceSelectorView model.dice ]
         , div
             [ style "clear" "both"
             , style "padding" "20px 12px"
@@ -92,17 +222,148 @@ view model =
         ]
 
 
-dieFaceImage : Int -> Html Msg
-dieFaceImage dieFace =
+{-| Show a representation of the dice MultiSelectRing.
+-}
+diceSelectorView : MultiSelectRing Die -> Html Msg
+diceSelectorView dice =
     let
-        dieFaceImagePath =
-            "../images/die-face-" ++ String.fromInt dieFace ++ ".jpg"
+        diceImages =
+            dice
+                |> MultiSelectRing.mapEachIntoList
+                    dieFaceImage
+                    (focusedDieFaceImage dice)
+                    selectedDieFaceImage
     in
-    div [ style "float" "left" ]
-        [ img
-            [ src dieFaceImagePath
-            , alt (String.fromInt dieFace)
-            , width 80
-            ]
-            []
+    div [] diceImages
+
+
+{-| Show a still representation of the provided item.
+-}
+dieFaceImage : Die -> Html Msg
+dieFaceImage die =
+    let
+        dieValue =
+            dieToString die
+    in
+    div
+        [ style "float" "left"
+        , style "cursor" "pointer"
+        , style "margin" "8px"
+        , style "padding" "2px"
         ]
+        [ a [ onClick (DieClicked die) ]
+            [ img
+                [ src ("../images/die-face-" ++ dieValue ++ ".jpg")
+                , alt dieValue
+                , title dieValue
+                , width 80
+                ]
+                []
+            ]
+        ]
+
+
+{-| Show a representation of the focused die that has been provided.
+-}
+focusedDieFaceImage : MultiSelectRing Die -> Die -> Html Msg
+focusedDieFaceImage dice focusedDie =
+    let
+        dieValue =
+            dieToString focusedDie
+    in
+    div
+        [ style "float" "left"
+        , style "border" "#b189f5 4px dashed"
+        , style "cursor" "pointer"
+        , style "margin" "8px"
+        , style "padding" "2px"
+        ]
+        [ a [ onClick (DieClicked focusedDie) ]
+            [ img
+                [ src ("../images/die-face-" ++ dieValue ++ ".jpg")
+                , alt dieValue
+                , title dieValue
+                , width 80
+                ]
+                []
+            ]
+        ]
+
+
+{-| Show a representation of the selected item that has been provided.
+-}
+selectedDieFaceImage : Die -> Html Msg
+selectedDieFaceImage selectedDie =
+    let
+        dieValue =
+            dieToString selectedDie
+    in
+    div
+        [ style "float" "left"
+        , style "border" "#a1a1a1 2px dashed"
+        , style "cursor" "pointer"
+        , style "margin" "8px"
+        , style "padding" "2px"
+        ]
+        [ a [ onClick (DieClicked selectedDie) ]
+            [ img
+                [ src ("../images/die-face-" ++ dieValue ++ ".jpg")
+                , alt dieValue
+                , title dieValue
+                , width 80
+                ]
+                []
+            ]
+        ]
+
+
+intToDie : Int -> Die
+intToDie value =
+    case value of
+        1 ->
+            One
+
+        2 ->
+            Two
+
+        3 ->
+            Three
+
+        4 ->
+            Four
+
+        5 ->
+            Five
+
+        6 ->
+            Six
+
+        _ ->
+            One
+
+
+dieToInt : Die -> Int
+dieToInt die =
+    case die of
+        One ->
+            1
+
+        Two ->
+            2
+
+        Three ->
+            3
+
+        Four ->
+            4
+
+        Five ->
+            5
+
+        Six ->
+            6
+
+
+dieToString : Die -> String
+dieToString die =
+    String.fromInt (dieToInt die)
